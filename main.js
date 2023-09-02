9b726eb7b8dd326b323215718a0d053616825f3d
ponmani36 <143865454+ponmani36@users.noreply.github.com>
2023-09-02 11:32:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var total=0;
+
+function addcount(){
+       
+    var name4=document.getElementById("count").value;
+    document.getElementById("labelname4").textContent = name4;
+}
+function total1(){
+       
+    var name5=document.getElementById("count2").value;
+    document.getElementById("labelname5").textContent = name5;
+}
+const num=0;
+   num=name4;
+document.write('hello $(num)');
+    
+function addname(){
+
+    
+        
+    var name1=document.getElementById("input1").value;
+    document.getElementById("labelname1").textContent = name1;
+
+    var name2=document.getElementById("input2").value;
+    document.getElementById("labelname2").textContent = name2;
+        
+    var name3=document.getElementById("input3").value;
+    document.getElementById("labelname3").textContent = name3;
+   
+    }
+  total=name3+total;
+
